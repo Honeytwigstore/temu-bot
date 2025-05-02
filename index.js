6394094da03e53a61ee9a6064b9a554afbcab70b
Honeytwigstore <spreeshopkc@gmail.com>
2025-05-02 14:30:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const puppeteer = require('puppeteer');
 
@@ -5,24 +6,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/run-temu-order', async (req, res) => {
-    try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
-        await page.goto('https://www.temu.com/');
-
-        console.log('Visited Temu homepage');
-
-        await browser.close();
-        res.send('Temu order script ran successfully!');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error running Temu order script.');
-    }
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.temu.com/');
+    await browser.close();
+    res.send('Temu order script ran successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error running Temu order script.');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
